6ef8f5eef36ea23606ced02c56278853fcc596a7
Don Freecs <freecsdon30@gmail.com>
2025-12-31 02:47:20 +0530

    feat: implemented argument parsing for host and port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+use std::net::TcpListener;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short = 'H', long)]
+    host: String,
+
+    #[arg(short, long)]
+    port: u16,
+}
 fn main() {
-    println!("this is a test")   
+    let args = Cli::parse();
+    let host = args.host;
+    let port = args.port;
+
+    let listener = TcpListener::bind(format!("{}:{}", host, port.to_string())).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("connection established..!!: {:?}", stream);
+    }
 }
